8ba871ba9f21120afca47f09338a689648fec116
Denis Friel <denis.friel@gmail.com>
2023-01-12 13:04:16 +0000

    first commit

---
+++ src/main/java/com/allstate/premierclaimsserver/PremierClaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.premierclaimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierClaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierClaimsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/premierclaimsserver/PremierClaimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.premierclaimsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierClaimsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
